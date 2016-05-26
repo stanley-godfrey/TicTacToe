f9383d6e32aba0fc237d7a409929156bd5df7228
Stanley Godfrey <stanley.godfrey@bourne-grammar.lincs.sch.uk>
2016-05-26 19:47:42 +0100

    Added 1 Player version to repository

---
+++ 1-Player-Pygame.py
@@ -0,0 +1,144 @@
+import pygame
+import sys
+import random
+import time
+
+
+# <editor-fold desc="Setup">
+pygame.init()
+pygame.mixer.quit()
+pygame.mixer.init()
+
+board_length = 3
+board_width = 3
+
+width = 50  # programming technique - variable assignment; data type - integer
+height = 50  # programming technique - variable assignment; data type - integer
+margin = 5  # programming technique - variable assignment; data type - integer
+
+clock = pygame.time.Clock()
+FPS = 60
+SCREENSIZE = SCREENWIDTH, SCREENHEIGHT = (board_length * width) + ((board_length + 1) * margin),\
+                                         (board_width * height) + ((board_width + 1) * margin)
+screen = pygame.display.set_mode(SCREENSIZE)
+
+off_white = (220, 220, 220)  # programming technique - variable assignment; data type - tuple
+black = (0, 0, 0)
+blue = (0, 0, 255)  # programming technique - variable assignment; data type - tuple
+green = (0, 255, 0)  # programming technique - variable assignment; data type - tuple
+
+gameState = "running"  # programming technique - variable assignment; data type - string
+gamePlayer = 'X'  # programming technique - variable assignment; data type - string
+
+board = []  # programming technique - variable assignment; data structure - list
+square = " "  # programming technique - variable assignment; data type - string
+wins = [[(0, 0), (0, 1), (0, 2)], [(1, 0), (1, 1), (1, 2)], [(2, 0), (2, 1), (2, 2)],
+        [(0, 0), (1, 0), (2, 0)], [(0, 1), (1, 1), (2, 1)], [(0, 2), (1, 2), (2, 2)],
+        [(0, 0), (1, 1), (2, 2)], [(0, 2), (1, 1), (2, 0)]]
+
+theme = pygame.mixer.music.load('bensound-buddy.mp3')
+pygame.mixer.music.play(-1)
+# </editor-fold>
+
+
+def generate_board(square_cell, x, y):
+    board_comp = [[square_cell for x in range(x)] for y in range(y)]
+    return board_comp
+
+
+def print_board(board):
+    print('-------')
+    for row in board:
+        print('|', end='')
+        for cell in row:
+            print(cell + '|', end='')
+        print()
+        print('-------')
+
+
+def check_win(board):
+    state = None
+    for win in wins:
+        state = board[win[0][0]][win[0][1]]
+        if board[win[1][0]][win[1][1]] == state and board[win[2][0]][win[2][1]] == state and state != ' ':
+            print('Win', state)
+            print_board(board)
+            return 'done'
+
+
+def check_full(board):
+    full = 0
+    for row in board:
+        for cell in row:
+            if cell != ' ':
+                full += 1
+    if full == 9:
+        print('Full')
+        print_board(board)
+        return 'full'
+
+
+def generate_move(board):
+    found = False
+    while not found:
+        move_x = random.choice(range(0, 3))
+        move_y = random.choice(range(0, 3))
+        if board[move_x][move_y] == ' ':
+            found = True
+    return move_x, move_y
+
+
+board = generate_board(square, board_length, board_width)
+
+start_time = time.time()
+
+screen.fill(black)
+
+# <editor-fold desc="Main Loop">
+while gameState != "exit":
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameState = "exit"
+        elif event.type == pygame.MOUSEBUTTONDOWN:  # programming technique - conditional elif
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_col = mouse_pos[0] // (width + margin)  # programming technique - integer division
+            mouse_row = mouse_pos[1] // (height + margin)  # programming technique - integer division
+            if board[mouse_row][mouse_col] == ' ':
+                board[mouse_row][mouse_col] = gamePlayer  # programming technique - two-dimensional arrays
+                if check_full(board) != 'full':
+                    x, y = generate_move(board)
+                    board[x][y] = 'O'
+
+    for row in range(0, board_length):  # programming technique - for loop
+        for column in range(0, board_width):  # programming technique - for loop
+            if board[row][column] == 'X':
+                color = green
+            elif board[row][column] == 'O':
+                color = blue
+            else:
+                color = off_white  # programming technique - variable assignment
+            pygame.draw.rect(screen, color, [(margin + width) * column + margin, (margin + height) * row + margin,
+                                             width, height])  # programming technique - pygame rectangle
+
+    if check_full(board) == 'full':
+        board = generate_board(square, board_length, board_width)
+        start_time = time.time()
+
+    pygame.display.flip()
+
+    win = check_win(board)
+    if win == 'done':
+        finish_time = time.time()
+        time_taken = finish_time - start_time
+        print('Time Taken: %s' % ('{0:.2f}'.format(time_taken)), end=' ')
+        print('seconds')
+        board = generate_board(square, board_length, board_width)
+        start_time = time.time()
+
+    clock.tick(FPS)
+# </editor-fold>
+
+pygame.mixer.music.stop()
+pygame.quit()
+sys.exit()
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # TicTacToe
 
 ##Information
-This is a, though basic, 2 Player Tic Tac Toe clone that I have made in Python at home over the February Half-Term.
-It is still not complete however, so if you do wish to make any changes of your own, then please feel free to.
+This is my own version of Tic-Tac-Toe / Noughts and Crosses. Currently, there are two versions in the repository:
+* 2-Player-Text.py is the two-player, console-based version of the game, This was the first version of the game and so there are likely many improvements that can be made to it
+* 1-Player-Pygame.py is the one-player, graphical version. It uses the pygame library for pygame to function. The AI in this game is completely random and follows no methodology.
+
+If you wish to make any improvements at all, please feel free to fork, commit and then make a pull request.
 
 ##Changelog
-* 14/02/2015 11:59 GMT - Finished the 2 Player python file, named main.py in the repo
+* 26/05/2016 19:44 GMT - Added the 1 player python file, 1-Player-Pygame.py in the repo.
+* 14/02/2015 11:59 GMT - Finished the 2 Player python file, 2-Player-Text.py in the repo.
 * 14/02/2015 09:25 GMT - Added the unfinished python file, which at the moment allows you to make an infinite number of moves   until you get a line.
